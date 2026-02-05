1bdfcd2ef7c44f9692cd7432349d21326abf6f0b
김도국 <kdk9208@naver.com>
2026-02-05 20:00:27 +0900

    5일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [person, setPerson] = useState({ name: 'John', age: 25 });
+  const [person, setPerson] = useState({ name: "John", age: 25 });
 
   const handleClick = () => {
-    person.age += 1; // 상태를 직접 변경
-    console.log(person); // 콘솔에서는 변경된 값이 보이지만, 화면에는 반영되지 않음
+    setPerson({ ...person, age: person.age + 1 });
   };
 
   return (
